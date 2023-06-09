873acb2d6ed4ad468c55ef2a94e6162684d0116e
terry677 <71567199+terry677@users.noreply.github.com>
2023-06-09 10:19:57 +0100

    initial commit updated

--- website/views.py
+++ website/views.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, render_template
-
+from .models import User
 views  = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return  render_template("home.html")
 
+@views.route('/users')
+def view_users():
+    # Retrieve all users from the database
+    users = User.query.all()
 
+    # Render a template to display the user data
+    return render_template('users.html', users=users)
