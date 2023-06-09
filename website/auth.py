6e78b6b66eada338a2ce967a5ae4b8853bafee7e
terry677 <71567199+terry677@users.noreply.github.com>
2023-06-09 07:12:37 +0100

    initial commit updated

--- website/auth.py
+++ website/auth.py
@@ -5,8 +5,6 @@ auth  = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template('login.html')
 
 
