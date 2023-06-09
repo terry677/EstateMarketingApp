b1dc5d483da5505a1f44be42cc22ec841399004b
terry677 <71567199+terry677@users.noreply.github.com>
2023-06-09 08:04:13 +0100

    initial commit updated

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,14 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, redirect, url_for
 from flask import request
+from .models import User, db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
+
+
+
+
 auth  = Blueprint('auth', __name__)
 
 
@@ -16,7 +25,8 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        name = request.form.get('name')
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
@@ -24,14 +34,19 @@ def sign_up():
 
         if len(email) < 4:
             flash('Email must be greater then 3 character.', category='error')
-        elif len(name) < 2:
+        elif len(firstname) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+        elif len(lastname) < 2:
             flash('First name must be greater than 2 characters.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+          new_user = User(email=email, firstname=firstname, lastname=lastname, password=generate_password_hash(password1, method='Sha256'))
+          db.session.add(new_user)
+          db.session.commit()
           flash('Account created!', category='success')
-
+          return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
--- website/models.py
+++ website/models.py
@@ -13,7 +13,8 @@ class Property(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    firstname = db.Column(db.String(50), unique=True, nullable=False)
+    lastname = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
