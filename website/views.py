9fb2baca2b01aaa7aea0aac1d8a3f5c0869e5dea
terry677 <71567199+terry677@users.noreply.github.com>
2023-06-09 23:26:58 +0100

    initial commitupdated

--- website/views.py
+++ website/views.py
@@ -8,17 +8,10 @@ from flask_login import login_user, logout_user, login_required, current_user
 views  = Blueprint('views', __name__)
 
 @views.route('/')
+def landing_page():
+    return render_template('landing.html')
+
+@views.route('/home')
 @login_required
 def home():
-    return  render_template("home.html")
-
-@views.route('/users')
-def view_users():
-    # Retrieve all users from the database
-    users = User.query.all()
-
-    # Get the current authenticated user, if any
-    user = current_user if current_user.is_authenticated else None
-
-    # Render a template to display the user data
-    return render_template('users.html', user=user)
+    return  render_template("home.html", user=current_user)
