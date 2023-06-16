06eb7c99895a78a5b3d6e5b19292711101904efb
terry abu <terryabu@terrys-MacBook-Air.local>
2023-06-16 17:40:51 +0100

    initial commit updated

--- website/models.py
+++ website/models.py
@@ -1,7 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from datetime import datetime
-from flask import url_for
+
 
 class Property(db.Model):
     id = db.Column(db.Integer, primary_key=True)
